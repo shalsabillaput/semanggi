63ae6ae99e688587f683f29d1ef83565898c5edb
bayusafutra <mochamad.bayu.safutra-2021@vokasi.unair.ac.id>
2023-04-18 22:26:33 +0700

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -17,7 +17,7 @@
     <!-- Style -->
     <link rel="stylesheet" href="css/stylelogin.css">
 
-    <title>Login Semanggi's Store</title>
+    <title>Login Semanggi Store</title>
   </head>
   <body>
 
